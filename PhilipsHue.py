2cc6db2ea5e3bf8510ad3c05d71e287e95ca9e28
Kevin Weirauch <kevin@weirau.ch>
2015-05-10 22:51:09 +0000

    Update with first pass of dimming

--- PhilipsHue.py
+++ PhilipsHue.py
@@ -6,7 +6,7 @@ import random
 
 b = Bridge('10.0.1.3') # Enter bridge IP here.
 
-WORDS = ["ON", "OFF", "OF", "TOGGLE", "KITCHEN", "BEDROOM"]
+WORDS = ["ON", "OFF", "OF", "TOGGLE", "KITCHEN", "BEDROOM", "DIM", "RANDOM"]
 
 lights = b.get_light_objects()
 
@@ -17,6 +17,7 @@ def handle(text, mic, profile):
 		if re.search(r'\bon\b', text, re.IGNORECASE):
 			mic.say("Turning on the kitchen lights.")
 			
+			b.set_group('Kitchen', 'bri', 250)
 			b.set_group('Kitchen', 'on', True)
 
 		elif re.search(r'\boff\b', text, re.IGNORECASE) or re.search(r'\bof\b', text, re.IGNORECASE):
@@ -30,17 +31,22 @@ def handle(text, mic, profile):
 	elif re.search(r'\bbedroom\b', text, re.IGNORECASE):
 
 		if re.search(r'\bon\b', text, re.IGNORECASE):
-			mic.say("Turning on the bedroom lights.")
+			mic.say("Turning on the bedroom light.")
 
 			b.set_group('Bedroom', 'on', True)
 
 		elif re.search(r'\boff\b', text, re.IGNORECASE) or re.search(r'\bof\b', text, re.IGNORECASE):
-			mic.say("Turning off the bedroom lights.")
+			mic.say("Turning off the bedroom light.")
 
 			b.set_group('Bedroom', 'on', False)
 
 		else:
 			mic.say("Sorry, I didn't understand that.")
+
+	elif re.search(r'\bdim\b', text, re.IGNORECASE):
+		mic.say("Dimming the lights.")
+
+		b.set_group('Kitchen', 'bri', 100, transitiontime=50)
 
 	elif re.search(r'\bon\b', text, re.IGNORECASE):
 		mic.say("Turning all lights on.")
@@ -68,6 +74,10 @@ def isValid(text):
 	elif re.search(r'\bkitchen\b', text, re.IGNORECASE):
 		return True
 	elif re.search(r'\bbedroom\b', text, re.IGNORECASE):
+		return True
+	elif re.search(r'\brandom\b', text, re.IGNORECASE):
+		return True
+	elif re.search(r'\bdim\b', text, re.IGNORECASE):
 		return True
 	else:
 		return False
