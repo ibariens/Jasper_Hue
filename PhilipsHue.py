46f9db30cc31ddf20e2fcedcfa047c22236e53fe
Kevin Weirauch <Kevin@Kevins-MacBook-Pro.local>
2015-05-10 21:21:37 -0400

    Cleaned up and implemented commands

--- PhilipsHue.py
+++ PhilipsHue.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 import random
 import re
-from phue import Bridge
 import random
+from phue import Bridge
 
 b = Bridge('10.0.1.3') # Enter bridge IP here.
 
-WORDS = ["ON", "OFF", "OF", "TOGGLE", "KITCHEN", "BEDROOM", "DIM", "RANDOM"]
+WORDS = ["ON", "OFF", "OF", "TOGGLE", "KITCHEN", "BEDROOM", "DIM"]
 
 lights = b.get_light_objects()
+
+#Commands for Phue
+on =  {'transistiontime' : 10, 'on' : True, 'bri' : 250}
+off = {'transitiontime' : 10, 'on' : False}
+dim = {'transitiontime' : 75, 'bri' : 75}
 
 def handle(text, mic, profile):
 
@@ -17,13 +22,12 @@ def handle(text, mic, profile):
 		if re.search(r'\bon\b', text, re.IGNORECASE):
 			mic.say("Turning on the kitchen lights.")
 			
-			b.set_group('Kitchen', 'bri', 250)
-			b.set_group('Kitchen', 'on', True)
+			b.set_group('Kitchen', on)
 
 		elif re.search(r'\boff\b', text, re.IGNORECASE) or re.search(r'\bof\b', text, re.IGNORECASE):
 			mic.say("Turning off the kitchen lights.")
 			
-			b.set_group('Kitchen', 'on', False)
+			b.set_group('Kitchen', off)
 
 		else:
 			mic.say("Sorry, I didn't understand that.")
@@ -33,12 +37,12 @@ def handle(text, mic, profile):
 		if re.search(r'\bon\b', text, re.IGNORECASE):
 			mic.say("Turning on the bedroom light.")
 
-			b.set_group('Bedroom', 'on', True)
+			b.set_group('Bedroom', on)
 
 		elif re.search(r'\boff\b', text, re.IGNORECASE) or re.search(r'\bof\b', text, re.IGNORECASE):
 			mic.say("Turning off the bedroom light.")
 
-			b.set_group('Bedroom', 'on', False)
+			b.set_group('Bedroom', off)
 
 		else:
 			mic.say("Sorry, I didn't understand that.")
@@ -46,7 +50,7 @@ def handle(text, mic, profile):
 	elif re.search(r'\bdim\b', text, re.IGNORECASE):
 		mic.say("Dimming the lights.")
 
-		b.set_group('Kitchen', 'bri', 100, transitiontime=50)
+		b.set_group('Kitchen', dim)
 
 	elif re.search(r'\bon\b', text, re.IGNORECASE):
 		mic.say("Turning all lights on.")
@@ -69,13 +73,13 @@ def isValid(text):
 		return True
 	elif re.search(r'\boff\b', text, re.IGNORECASE):
 		return True
+	elif re.search(r'\bof\b', text, re.IGNORECASE):
+		return True
 	elif re.search(r'\btoggle\b', text, re.IGNORECASE):
 		return True
 	elif re.search(r'\bkitchen\b', text, re.IGNORECASE):
 		return True
 	elif re.search(r'\bbedroom\b', text, re.IGNORECASE):
-		return True
-	elif re.search(r'\brandom\b', text, re.IGNORECASE):
 		return True
 	elif re.search(r'\bdim\b', text, re.IGNORECASE):
 		return True
