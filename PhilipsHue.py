a9feeeec97a96f144573daffc7b00408498e1c26
Kevin Weirauch <kevin@weirau.ch>
2015-05-10 22:01:14 -0400

    Further fubar indents

--- PhilipsHue.py
+++ PhilipsHue.py
@@ -17,72 +17,72 @@ dim = {'transitiontime' : 75, 'bri' : 75}
 
 def handle(text, mic, profile):
 
-	if re.search(r'\bkitchen\b', text, re.IGNORECASE):
+    if re.search(r'\bkitchen\b', text, re.IGNORECASE):
 
-	  if re.search(r'\bon\b', text, re.IGNORECASE):
-			mic.say("Turning on the kitchen lights.")
-			
-			b.set_group('Kitchen', on)
+      if re.search(r'\bon\b', text, re.IGNORECASE):
+            mic.say("Turning on the kitchen lights.")
+            
+            b.set_group('Kitchen', on)
 
-		elif re.search(r'\boff\b', text, re.IGNORECASE) or re.search(r'\bof\b', text, re.IGNORECASE):
-			mic.say("Turning off the kitchen lights.")
-			
-			b.set_group('Kitchen', off)
+        elif re.search(r'\boff\b', text, re.IGNORECASE) or re.search(r'\bof\b', text, re.IGNORECASE):
+            mic.say("Turning off the kitchen lights.")
+            
+            b.set_group('Kitchen', off)
 
-		else:
-			mic.say("Sorry, I didn't understand that.")
+        else:
+            mic.say("Sorry, I didn't understand that.")
 
-	elif re.search(r'\bbedroom\b', text, re.IGNORECASE):
+    elif re.search(r'\bbedroom\b', text, re.IGNORECASE):
 
-		if re.search(r'\bon\b', text, re.IGNORECASE):
-			mic.say("Turning on the bedroom light.")
+        if re.search(r'\bon\b', text, re.IGNORECASE):
+            mic.say("Turning on the bedroom light.")
 
-			b.set_group('Bedroom', on)
+            b.set_group('Bedroom', on)
 
-		elif re.search(r'\boff\b', text, re.IGNORECASE) or re.search(r'\bof\b', text, re.IGNORECASE):
-			mic.say("Turning off the bedroom light.")
+        elif re.search(r'\boff\b', text, re.IGNORECASE) or re.search(r'\bof\b', text, re.IGNORECASE):
+            mic.say("Turning off the bedroom light.")
 
-			b.set_group('Bedroom', off)
+            b.set_group('Bedroom', off)
 
-		else:
-			mic.say("Sorry, I didn't understand that.")
+        else:
+            mic.say("Sorry, I didn't understand that.")
 
-	elif re.search(r'\bdim\b', text, re.IGNORECASE):
-		mic.say("Dimming the lights.")
+    elif re.search(r'\bdim\b', text, re.IGNORECASE):
+        mic.say("Dimming the lights.")
 
-		b.set_group('Kitchen', dim)
+        b.set_group('Kitchen', dim)
 
-	elif re.search(r'\bon\b', text, re.IGNORECASE):
-		mic.say("Turning all lights on.")
+    elif re.search(r'\bon\b', text, re.IGNORECASE):
+        mic.say("Turning all lights on.")
 
-		for light in lights:
-			light.on = True
+        for light in lights:
+            light.on = True
 
-	elif re.search(r'\boff\b', text, re.IGNORECASE) or re.search(r'\bof\b', text, re.IGNORECASE):
+    elif re.search(r'\boff\b', text, re.IGNORECASE) or re.search(r'\bof\b', text, re.IGNORECASE):
 
-		mic.say("Turning lights off.")
+        mic.say("Turning lights off.")
 
-		for light in lights:
-			light.on = False
+        for light in lights:
+            light.on = False
 
-	else:
-		mic.say("I didn't understand that.")
-	
+    else:
+        mic.say("I didn't understand that.")
+    
 def isValid(text):
   
-	if re.search(r'\bon\b', text, re.IGNORECASE):
-		return True
-	elif re.search(r'\boff\b', text, re.IGNORECASE):
-		return True
-	elif re.search(r'\bof\b', text, re.IGNORECASE):
-		return True
-	elif re.search(r'\btoggle\b', text, re.IGNORECASE):
-		return True
-	elif re.search(r'\bkitchen\b', text, re.IGNORECASE):
-		return True
-	elif re.search(r'\bbedroom\b', text, re.IGNORECASE):
-		return True
-	elif re.search(r'\bdim\b', text, re.IGNORECASE):
-		return True
-	else:
-		return False
+    if re.search(r'\bon\b', text, re.IGNORECASE):
+        return True
+    elif re.search(r'\boff\b', text, re.IGNORECASE):
+        return True
+    elif re.search(r'\bof\b', text, re.IGNORECASE):
+        return True
+    elif re.search(r'\btoggle\b', text, re.IGNORECASE):
+        return True
+    elif re.search(r'\bkitchen\b', text, re.IGNORECASE):
+        return True
+    elif re.search(r'\bbedroom\b', text, re.IGNORECASE):
+        return True
+    elif re.search(r'\bdim\b', text, re.IGNORECASE):
+        return True
+    else:
+        return False
